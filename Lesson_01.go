e8bb426a44cdbe09f4268762cf23318627e099f8
denisakov <denis.isakov@tut.by>
2021-07-09 18:03:07 +0300

    Lesson 01 (#1)
    * Clean up
    
    * Homework 01
    
    * Corrections
    
    Co-authored-by: Denis Isakov <denisakov@gmail.com>

--- Lesson_01.go
+++ Lesson_01.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji"
 )
 
 func main() {
-	emoji.Println(":smile: Beer!!!")
-	worldMssg := emoji.Sprint("Hello world!:smile:")
-	fmt.Println(worldMssg)
+	emoji.Println("😄Hello world!:smile:")
 }
